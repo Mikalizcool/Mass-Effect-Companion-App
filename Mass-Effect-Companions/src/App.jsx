190a8a522795730f842d7fc989d5cc768cc02db9
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-04-16 11:50:37 -0400

    feat: create footer

--- Mass-Effect-Companions/src/App.jsx
+++ Mass-Effect-Companions/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <>
-      <p>Throughout the Milky Way and Andromeda Galaxies, we encounter several companions.</p>
+      <p>Throughout the Milky Way Galaxy, we encounter several companions...</p>
       <div className="container">
         <div className="characterProfile">
           <h2>Garrus Vakarian</h2>
@@ -84,27 +84,6 @@ function App() {
         </div>
       </div>
 
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- Mass-Effect-Companions/src/main.jsx
+++ Mass-Effect-Companions/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Logo from './Logo.jsx'
+import Footer from './Footer.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Logo />
     <App />
+    <Footer />
   </React.StrictMode>,
 )
