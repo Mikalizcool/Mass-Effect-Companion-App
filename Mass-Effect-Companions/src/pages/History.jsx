bd327f072907bf5c17ed9d59530d0b6648b6bfc7
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-04-18 11:46:39 -0400

    feat: add background image

--- Mass-Effect-Companions/src/pages/History.jsx
+++ Mass-Effect-Companions/src/pages/History.jsx
@@ -4,8 +4,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { UseBackgroundImage } from "../hooks/designhooks";
+import history from '../assets/history.jpg'
 function History () {
-    UseBackgroundImage('');
+    UseBackgroundImage(history);
     return (
         <p> hi</p>
     )
