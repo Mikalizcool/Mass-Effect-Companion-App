2ead0c29b23bb4e5eef5a34d237ec2b9fac5e18c
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-04-23 13:15:29 -0400

    style: change history container color

--- Mass-Effect-Companions/src/pages/History.jsx
+++ Mass-Effect-Companions/src/pages/History.jsx
@@ -16,10 +16,10 @@ function History () {
             <Navbar />
             <div className={style.container}>
                 <h1>History of Mass Effect</h1>
-                <p>In the year 2148, explorers on Mars discovered the remains of an ancient spacefaring civilization. In the decades that followed, these mysterious artifacts revealed startling new technologies, enabling travel to the furthest stars. The basis for this incredible technology was a force that controlled the very fabric of space and time.
-                    <br></br><br></br>They called it the greatest discovery in human history.
-                    <br></br><br></br>The civilizations of the galaxy call it... MASS EFFECT.
-                </p>
+                    <p className={style.pcontainer}>In the year 2148, explorers on Mars discovered the remains of an ancient spacefaring civilization. In the decades that followed, these mysterious artifacts revealed startling new technologies, enabling travel to the furthest stars. The basis for this incredible technology was a force that controlled the very fabric of space and time.
+                        <br></br><br></br>They called it the greatest discovery in human history.
+                        <br></br><br></br>The civilizations of the galaxy call it... MASS EFFECT.
+                    </p>
             </div>
         </>
     )
