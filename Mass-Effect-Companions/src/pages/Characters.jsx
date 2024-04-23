80fd3eabe2d05faf4be404493999be9788987db6
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-04-23 18:02:36 -0400

    feat: change css file to module css file

--- Mass-Effect-Companions/src/pages/Characters.jsx
+++ Mass-Effect-Companions/src/pages/Characters.jsx
@@ -9,78 +9,78 @@ import jackProfile from '../assets/jack-profile.jpg'
 import mirandaProfile from '../assets/miranda-profile.jpg'
 import { UseBackgroundImage } from '../hooks/designhooks';
 import companions from '../assets/companions.jpg'
-import './Characters.css'
+import style from './Characters.module.css'
 import Navbar from '../components/Navbar'
 function Characters () {
     UseBackgroundImage();
     return (
         <>
             <Navbar />
-            <p>Throughout the Milky Way Galaxy, we encounter several companions...</p>
-            <div className="container">
-                <div className="characterProfile">
+            <p className={style.intro}>Throughout the Milky Way Galaxy, we encounter several companions...</p>
+            <div className={style.container}>
+                <div className={style.characterProfile}>
                 <h2>Garrus Vakarian</h2>
                 <Link to="Garrus">
                     <img
                     src = {garrusProfile}
                     alt = "Kaiden Alenko"
-                    className = "characterImage"
+                    className = {style.characterImage}
                     />
                 </Link>
                 </div>
-                <div className="characterProfile">
+                <div className={style.characterProfile}>
                 <h2>Urdnot Wrex</h2>
                 <a href="https://mikalizcool.com" target="_blank">
                     <img
                     src = {wrexProfile}
                     alt = "Kaiden Alenko"
-                    className = "characterImage"
+                    className = {style.characterImage}
                     />
                 </a>
                 </div>
             </div>
 
-            <div className="container">
-            <div className="characterProfile">
+            <div className={style.container}>
+            <div className={style.characterProfile}>
                 <h2>Liara Tsoni</h2>
                 <a href="https://mikalizcool.com" target="_blank">
                     <img
                     src = {liaraProfile}
                     alt = "Kaiden Alenko"
-                    className = "characterImage"
+                    className = {style.characterImage}
                     />
                 </a>
                 </div>
-                <div className="characterProfile">
+                <div className={style.characterProfile}>
                 <h2>Tali Zorah</h2>
                 <a href="https://mikalizcool.com" target="_blank">
                     <img
                     src = {taliProfile}
                     alt = "Kaiden Alenko"
-                    className = "characterImage"
+                    className = {style.characterImage}
                     />
                 </a>
                 </div>
             </div>
 
-            <div className="container">
-            <div className="characterProfile">
+            <div className={style.container}>
+            <div className={style.characterProfile}>
                 <h2>Jack</h2>
                 <a href="https://mikalizcool.com" target="_blank">
                     <img
                     src = {jackProfile}
                     alt = "Kaiden Alenko"
-                    className = "characterImage"
+                    className = {style.characterImage}
                     />
                 </a>
                 </div>
-                <div className="characterProfile">
+                <div className={style.characterProfile}>
                 <h2>Miranda</h2>
                 <a href="https://mikalizcool.com" target="_blank">
                     <img
                     src = {mirandaProfile}
                     alt = "Kaiden Alenko"
-                    className = "characterImage"
+                    className = {style.characterImage}
                     />
                 </a>
                 </div>
