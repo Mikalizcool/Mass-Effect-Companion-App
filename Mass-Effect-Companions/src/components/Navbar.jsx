8cbecc090a9109e3c0aaf387e810d2b925e80f1e
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-04-23 12:24:21 -0400

    style: improve navbar layout

--- Mass-Effect-Companions/src/components/Navbar.jsx
+++ Mass-Effect-Companions/src/components/Navbar.jsx
@@ -4,14 +4,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Outlet, Link } from "react-router-dom";
 
+import Logo from '../assets/Mass_Effect_logo.png'
+
 import style from './Navbar.module.css'
 function Navbar () {
     return (
         <>
-            <ul className={style.ulcontainer}>
-                <li className={style.lidesign}><Link to="/">Home</Link></li>
-                <li className={style.lidesign}><Link to="/history">History</Link></li>
-                <li className={style.lidesign}><Link to="/companions">Companions</Link></li>
+            <ul className={style.navbar}>
+                <li className={style.navbarleft}><Link to="/"><img className={style.logo} src={Logo}></img></Link></li>
+                <div className={style.navbarright}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/history">History</Link></li>
+                    <li><Link to="/companions">Companions</Link></li>
+                </div>
             </ul>
         </>
     )
