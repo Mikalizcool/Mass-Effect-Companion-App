67270774988e059324f6b85d1bfe315bcaa8b578
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-03 12:26:59 -0400

    feat: add tailwind css

--- Mass-Effect-Companion-App/src/components/Navbar.jsx
+++ Mass-Effect-Companion-App/src/components/Navbar.jsx
@@ -6,6 +6,10 @@ import { Outlet, Link } from "react-router-dom";
 
 import Logo from '../assets/Mass_Effect_logo.png'
 
+import symbol from '../assets/menu_FILL0_wght400_GRAD0_opsz24.svg'
+
+import { navbarAppear } from '../hooks/designhooks'
+
 import style from './Navbar.module.css'
 function Navbar () {
     return (
@@ -13,9 +17,10 @@ function Navbar () {
             <ul className={style.navbar}>
                 <li className={style.navbarleft}><Link to="/"><img className={style.logo} src={Logo}></img></Link></li>
                 <div className={style.navbarright}>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/history">History</Link></li>
-                    <li><Link to="/companions">Companions</Link></li>
+                    <li className={style.symbol}><button type="button" onClick={navbarAppear()}><img src={symbol}></img></button></li>
+                    <li className={style.navelements}><Link to="/">Home</Link></li>
+                    <li className={style.navelements}><Link to="/history">History</Link></li>
+                    <li className={style.navelements}><Link to="/companions">Companions</Link></li>
                 </div>
             </ul>
         </>
--- Mass-Effect-Companion-App/src/hooks/designhooks.js
+++ Mass-Effect-Companion-App/src/hooks/designhooks.js
@@ -1,8 +1,17 @@
 import { useState, useEffect } from "react";
 
+import Navbar from '../components/Navbar'
+
 export function UseBackgroundImage(image) {
   useEffect(() => {
     document.body.style.backgroundImage = `url(${image})`;
     document.body.style.backgroundSize = 'cover';
+  }, []);
+}
+
+var a = document.getElementsByClassName("navelement");
+export function navbarAppear() {
+  useEffect(() => {
+    a.display = "none";
   }, []);
 }
